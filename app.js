ed6f58352d11bbb5b1e7710fdfe7ca7c511bf10c
Colette Molleur <colette.molleur@gmail.com>
2016-05-09 15:34:08 -0400

    Adds updated master folder

--- app.js
+++ app.js
@@ -1,18 +1,46 @@
-var express     = require('express');
-var morgan      = require('morgan');
-var mongoose    = require('mongoose');
-var bodyParser  = require('body-parser');
-var app = express();
+var dotEnv          = require('dotenv').config(),
+    express         = require('express'),
+    morgan          = require('morgan'),
+    mongoose        = require('mongoose'),
+    bodyParser      = require('body-parser'),
+    cookieParser    = require('cookie-parser'),
+    app             = express(),
+    indexRouter     = require('./server/routes/index.js'),
+    apiAuthRouter   = require('./server/routes/api/auth.js'),
+    apiUsersRouter  = require('./server/routes/api/users.js');
 
 app.set('view engine', 'ejs');
 
-var indexRouter = require('./routes/index');
+// connect to db
+// process.env.MONGOLAB_URI is needed for when we deploy to Heroku
+mongoose.connect( process.env.MONGOLAB_URI || "mongodb://localhost/auth_template_app" );
+
+// log requests to STDOUT
+app.use(morgan('dev'));
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// parse application/json
+app.use(bodyParser.json());
+
+var indexRouter = require('./server/routes/index.js');
 app.use('/', indexRouter);
 
-var weatherRouter = require('./routes/weather');
+var weatherRouter = require('./server/routes/weather.js');
 app.use('/weather', weatherRouter);
 
-app.use(express.static('./public'));
+var userRouter = require('./server/routes/user.js');
+app.use('/user', userRouter);
+
+app.use(cookieParser());
+
+// Set static file root folder
+app.use(express.static('client/public'));
+
+app.use('/', indexRouter);
+app.use('/api/auth', apiAuthRouter);
+app.use('/api/users', apiUsersRouter);
 
 app.listen( 8080, function(){
   console.log( '... awaiting connections on port: 8080' );
