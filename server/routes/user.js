4d7bb3718cde5a22d244016d2a9b8388f3d76b46
risurisu <lizdorval@gmail.com>
2016-05-10 13:46:04 -0400

    added jwt to user.js in server folder. commented out for review

--- server/routes/user.js
+++ server/routes/user.js
@@ -11,3 +11,26 @@ router.get('/', function(req, res, next) {
 });
 
 module.exports = router;
+
+// adding jwt template for authorization.
+var express = require("express");
+var usersRouter = express.Router();
+var User = require("../../models/user");
+// var tokenAuthMiddleware = require('../../lib/authorizeRoute.js');
+var passport = require("../../lib/passportStrategy.js");
+
+usersRouter.post("/", function(req, res){
+  User.create( req.body.user, function( err, dbUser ){
+      if(err){ console.log(err); } //  dont do this
+
+      res.json( dbUser );
+  });
+});
+
+//any route above this line isnt protected
+usersRouter.use( passport.authenticate("jwt", { session: false} ) );
+
+usersRouter.get("/", function(req, res){});
+
+
+module.exports = usersRouter;
